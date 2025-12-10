cf3eac48ab72bdd71069efb8c9ce6cc581244aa4
Sara-inam <sarainam60@gmail.com>
2025-12-10 16:09:55 +0500

    search

--- app/api/employee/search/route.js
+++ app/api/employee/search/route.js
@@ -4,24 +4,16 @@ import { verifyAdmin } from "../../../libs/verifyAdmin.js";
 
 export const GET = verifyAdmin(async (req) => {
   const { searchParams } = new URL(req.url);
-  const query = searchParams.get("query")?.trim() || "";
+  const query = searchParams.get("query") || "";
 
-  if (!query) {
-    // agar empty query, return empty array
-    return NextResponse.json({ employees: [] });
-  }
-
-  // Max 50 results to avoid overload
   const employees = await User.find({
     role: "employee",
     isDeleted: false,
     $or: [
-      { email: { $regex: `^${query}`, $options: "i" } },
-      { name: { $regex: `^${query}`, $options: "i" } }
+      { email: { $regex: query, $options: "i" } },
+      { name: { $regex: query, $options: "i" } }
     ]
-  })
-    .select("_id name email")
-    .limit(50);
+  }).select("_id name email");
 
   return NextResponse.json({ employees });
 });
